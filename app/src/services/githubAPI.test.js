14e512401ebbfb0b51bf1cdba99dfe20fb435548
G.Provost <grace.g.provost@gmail.com>
2018-05-29 17:16:54 -0700

    start testing of githubAPI

---
+++ app/src/services/githubAPI.test.js
@@ -0,0 +1,42 @@
+jest.mock('./request', () => ({
+  getCORS: jest.fn(() => Promise.resolve({}))
+}));
+jest.mock('./githubAPI', () => ({
+  getRepos: jest.fn(() => Promise.resolve([])),
+  getLanguagesAndLibraries: jest.fn(() => Promise.resolve({}))
+}));
+
+import { getLanguagesAndLibraries } from './githubAPI';
+import 'isomorphic-fetch';
+
+describe('languages and libraries async spec', () => {
+
+  const message = 'message';
+  const mockResponse = (status, statusText, response) => {
+    return new Response(response, {
+      status: status,
+      statusText: statusText,
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+  };
+
+  it('succeeds a call', async() => {
+    const fakePromise = Promise.resolve(mockResponse(200, null, JSON.stringify({ message })));
+    
+    getLanguagesAndLibraries.mockImplementationOnce(() => fakePromise);
+
+    getLanguagesAndLibraries();
+
+    await Promise.all([fakePromise]);
+    
+    expect.assertions(2);
+
+    expect(getLanguagesAndLibraries).toHaveBeenCalled();
+    expect(getLanguagesAndLibraries.mock.calls[0]).toEqual([]);
+  });
+
+
+
+});
