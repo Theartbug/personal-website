4f3f10cb6c2c8aaa2a6ed7b6cf59d5fa08164947
G.Provost <grace.g.provost@gmail.com>
2018-06-01 14:58:29 -0700

    testing works on most of githubAPI

--- app/src/services/githubAPI.js
+++ app/src/services/githubAPI.js
@@ -67,9 +67,9 @@ export const findLibraries = async(repos) => {
   
 };
 
-export const getRepoContent = (repoName) => getCORS(`${BASE_URL}/repos/theartbug/${repoName}/git/trees/master?recursive=1`, options);
+export const getRepoContent = (repoName) => getCORS(`${BASE_URL}/repos/theartbug/${repoName}/git/trees/master?recursive=1`, options).catch(err => console.log(err));
 
-export const getPackageJson = (url) => getCORS(url, options);
+export const getPackageJson = (url) => getCORS(url, options).catch(err => console.log(err));
 
 export const findLanguages = async(repos) => {
   const seen = {};
