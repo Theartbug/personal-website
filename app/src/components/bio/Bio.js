127a4e401e09885afc9fcaf5bc073dbaf18c432b
G.Provost <grace.g.provost@gmail.com>
2018-06-22 11:43:38 -0700

    complete bio and begin skills

--- app/src/components/bio/Bio.js
+++ app/src/components/bio/Bio.js
@@ -7,12 +7,12 @@ export default class Hero extends PureComponent {
 
     return (
       <section className="bio">
-        <p>She <span>Enjoys Her Work</span>: there is a deep satisfaction to debugging, pushing pixels, and creating elegant logic</p>
+        <p>She <span>Enjoys Her Work</span>: there is a deep satisfaction to debugging, pushing pixels, and creating elegant logic.</p>
         <p>She is <span>Determined</span>: coming from a background in medicine; plunging into the magnificent world of tech.</p>
         <p>She is <span>Curious</span>: perpetually reading, discovering small tidbits of knowledge, and never failing to ask questions.</p>
         <p>She is <span>Active</span>: enjoying bike commuting, regular rock climbing, dancing, hiking, camping, and urban walks.</p>
+        <p>She is <span>Currently</span>: a frontend developer, spending the majority of her time building a video-based review app for a startup.</p>
       </section>
-      
     );
   }
 }
---
+++ app/src/components/skills/Skills.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './bio.css';
+
+export default class Hero extends PureComponent {
+
+  render() {
+
+    return (
+      <section className="skills">
+        
+      </section>
+    );
+  }
+}
