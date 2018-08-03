a9b36fbfb6364b2556d64b5ee813b4447e9169af
G.Provost <grace.g.provost@gmail.com>
2018-08-03 13:57:54 -0700

    attempt intersection observer polyfill

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import 'intersection-observer';
 import App from './components/app/App';
 import './styles/reset.css';
 import './styles/main.css';
