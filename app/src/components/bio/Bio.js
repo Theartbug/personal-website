17573967b4fd393d58ce29df983a9c95c0151aa5
G.Provost <grace.g.provost@gmail.com>
2018-08-03 15:57:07 -0700

    create a grid for bio

--- app/src/components/bio/Bio.js
+++ app/src/components/bio/Bio.js
@@ -28,29 +28,29 @@ export default class Hero extends PureComponent {
     return (
       <section className="bio" id="bio" ref={bio => this.bio = bio}>
         <h2 className="lines">About</h2>
-        <div>
-          <BikeIcon/>
-          <p>She is <span>Active</span> and enjoys bike commuting, rock climbing, dancing, hiking, camping, and urban walks.</p>
+        <div className="grid">
+          <div>
+            <BikeIcon/>
+            <p>She is <span>Active</span> and enjoys bike commuting, rock climbing, dancing, hiking, camping, and urban walks.</p>
+          </div>
+          <div>
+            <BookIcon/>
+            <p>She is <span>Curious</span> and is perpetually reading, discovering small tidbits of knowledge, and never failing to ask questions.</p>
+          </div>
+          <div>
+            <PlantIcon/>
+            <p>She is <span>Creative</span> and enjoys crafting, sewing, and cultivating her many houseplants.</p>
+          </div>
+          <div>
+            <MedIcon/>
+            <p>She is <span>Determined</span> and comes from a background in medicine; now plunging into the magnificent world of tech.</p>
+          </div>
+          <div>
+            <CodeIcon/>
+            <p>She is <span>Currently</span> a software engineer at Marketo. She enjoys her work and has a deep satisfaction in debugging, pushing pixels, and creating elegant logic.</p>
+          </div>
         </div>
-        <div>
-          <BookIcon/>
-          <p>She is <span>Curious</span> and is perpetually reading, discovering small tidbits of knowledge, and never failing to ask questions.</p>
-        </div>
-        <div>
-          <PlantIcon/>
-          <p>She is <span>Creative</span> and enjoys crafting, sewing, and cultivating her many houseplants.</p>
-        </div>
-        <div>
-          <MedIcon/>
-          <p>She is <span>Determined</span> and comes from a background in medicine; now plunging into the magnificent world of tech.</p>
-        </div>
-        <div>
-          <CodeIcon/>
-          <p>She is <span>Currently</span> a software engineer at Marketo. She enjoys her work and has a deep satisfaction in debugging, pushing pixels, and creating elegant logic.</p>
-        </div>
-        <div>
 
-        </div>
       </section>
     );
   }
