49d63fdcf628d8d95aa55c247d436c5b1a0ec6da
G.Provost <grace.g.provost@gmail.com>
2018-08-03 15:42:15 -0700

    add an icon to each section in bio

--- app/src/components/bio/Bio.js
+++ app/src/components/bio/Bio.js
@@ -1,4 +1,10 @@
 import React, { PureComponent } from 'react';
+import BikeIcon from 'react-icons/lib/fa/bicycle';
+import BookIcon from 'react-icons/lib/fa/book';
+import MedIcon from 'react-icons/lib/io/ios-medkit';
+import GradIcon from 'react-icons/lib/fa/graduation-cap';
+import CodeIcon from 'react-icons/lib/fa/code';
+import PlantIcon from 'react-icons/lib/fa/pagelines';
 import './bio.css';
 
 export default class Hero extends PureComponent {
@@ -22,11 +28,29 @@ export default class Hero extends PureComponent {
     return (
       <section className="bio" id="bio" ref={bio => this.bio = bio}>
         <h2 className="lines">About</h2>
-        <p>She is <span>Active</span> and enjoys bike commuting, rock climbing, dancing, hiking, camping, and urban walks.</p>
-        <p>She is <span>Curious</span> and is perpetually reading, discovering small tidbits of knowledge, and never failing to ask questions.</p>
-        <p>She is <span>Determined</span> and comes from a background in medicine; now plunging into the magnificent world of tech.</p>
-        <p>She is <span>Currently</span> a frontend developer, spending the majority of her time building a video-based review app for a startup.</p>
-        <p>She <span>Enjoys Her Work</span> and has a deep satisfaction in debugging, pushing pixels, and creating elegant logic.</p>
+        <div>
+          <BikeIcon/>
+          <p>She is <span>Active</span> and enjoys bike commuting, rock climbing, dancing, hiking, camping, and urban walks.</p>
+        </div>
+        <div>
+          <BookIcon/>
+          <p>She is <span>Curious</span> and is perpetually reading, discovering small tidbits of knowledge, and never failing to ask questions.</p>
+        </div>
+        <div>
+          <PlantIcon/>
+          <p>She is <span>Creative</span> and enjoys crafting, sewing, and cultivating her many houseplants.</p>
+        </div>
+        <div>
+          <MedIcon/>
+          <p>She is <span>Determined</span> and comes from a background in medicine; now plunging into the magnificent world of tech.</p>
+        </div>
+        <div>
+          <CodeIcon/>
+          <p>She is <span>Currently</span> a software engineer at Marketo. She enjoys her work and has a deep satisfaction in debugging, pushing pixels, and creating elegant logic.</p>
+        </div>
+        <div>
+
+        </div>
       </section>
     );
   }
