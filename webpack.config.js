f2cf735ed2d9d63f42a7bfd34e704b05badca25f
G.Provost <grace.g.provost@gmail.com>
2018-12-09 14:30:45 -0800

    try again

--- webpack.config.js
+++ webpack.config.js
@@ -6,74 +6,79 @@ const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = `${__dirname}/build`;
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = env => {
-  return {
-    entry: './app/src/main.js',
-    output: {
-      path,
-      filename: 'bundle.[hash].js',
-      publicPath: ''
-    },
-    devServer: {
-      contentBase: './build',
-      historyApiFallback: true
-    },
-    devtool: env.NODE_ENV === 'production' ?
-      'inline-source-map' :
-      'cheap-eval-source-map',
-    plugins: [
-      new ExtractTextPlugin('styles.css'),
-      new CleanWebpackPlugin(`${path}/bundle.*.js`),
-      new HtmlPlugin({ template: './app/src/index.html' }),
-      new Dotenv({
-        systemvars: true //allows netlify system variables to work in production
-      }),
-      new CopyWebpackPlugin([
-        // relative path is from src
-        { from: './app/src/assets/favicon.ico' }
-      ])
-    ],
-    module: {
-      rules: [
-        {   
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: env.NODE_ENV === 'production' ?
-                ExtractTextPlugin.extract({
-                  fallback: 'style-loader',
-                  use: 'css-loader'
-                }) :
-                'style-loader',
-              options: { sourceMap: true }
-            },
-            {
-              loader: 'css-loader',
-              options: { 
-                sourceMap: true,
-                importLoaders: 1 
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: { sourceMap: true }
+module.exports = {
+  entry: './app/src/main.js',
+  output: {
+    path,
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+  devServer: {
+    contentBase: './build',
+    historyApiFallback: true
+  },
+  devtool: process.env.NODE_ENV === 'production' ?
+    'inline-source-map' :
+    'cheap-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        // defaults the environment to development if not specified
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      }
+    }),
+    new ExtractTextPlugin('styles.css'),
+    new CleanWebpackPlugin(`${path}/bundle.*.js`),
+    new HtmlPlugin({ template: './app/src/index.html' }),
+    new Dotenv({
+      systemvars: true //allows netlify system variables to work in production
+    }),
+    new CopyWebpackPlugin([
+      // relative path is from src
+      { from: './app/src/assets/favicon.ico' }
+    ])
+  ],
+  module: {
+    rules: [
+      {   
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: process.env.NODE_ENV === 'production' ?
+              ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'css-loader'
+              }) :
+              'style-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'css-loader',
+            options: { 
+              sourceMap: true,
+              importLoaders: 1 
             }
-          ]
-        },
-        {
-          test: /\.(pdf|jpg|png|gif|svg|ico)$/,
-          use: {
-            loader: 'url-loader',
-            options: { limit: 5000 },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true }
           }
+        ]
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 5000 },
         }
-      ]
-    }
-  };
+      }
+    ]
+  }
 };
