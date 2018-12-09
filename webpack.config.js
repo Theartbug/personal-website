5e9390cf6234301c847a322e1a3dc14c1f1ff356
G.Provost <grace.g.provost@gmail.com>
2018-12-09 13:36:35 -0800

    see if adding extract text plugin works

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,79 @@
 /* eslint-env node */
 
-//remember to bring back in Extract-text-plugin for production to fix flash of unstyled code
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = `${__dirname}/build`;
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './app/src/main.js',
-  output: {
-    path,
-    filename: 'bundle.[hash].js',
-    publicPath: ''
-  },
-  devServer: {
-    contentBase: './build',
-    historyApiFallback: true
-  },
-  devtool: 'inline-source-map',
-  plugins: [
-    new CleanWebpackPlugin(`${path}/bundle.*.js`),
-    new HtmlPlugin({ template: './app/src/index.html' }),
-    new Dotenv({
-      systemvars: true //allows netlify system variables to work in production
-    }),
-    new CopyWebpackPlugin([
-      // relative path is from src
-      { from: './app/src/assets/favicon.ico' }
-    ])
-  ],
-  module: {
-    rules: [
-      {   
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-            options: { sourceMap: true }
-          },
-          {
-            loader: 'css-loader',
-            options: { 
-              sourceMap: true,
-              importLoaders: 1 
+module.exports = env => {
+  return {
+    entry: './app/src/main.js',
+    output: {
+      path,
+      filename: 'bundle.[hash].js',
+      publicPath: ''
+    },
+    devServer: {
+      contentBase: './build',
+      historyApiFallback: true
+    },
+    devtool: env.NODE_ENV === 'production' ?
+      'inline-source-map' :
+      'cheap-eval-source-map',
+    plugins: [
+      new ExtractTextPlugin('styles.css'),
+      new CleanWebpackPlugin(`${path}/bundle.*.js`),
+      new HtmlPlugin({ template: './app/src/index.html' }),
+      new Dotenv({
+        systemvars: true //allows netlify system variables to work in production
+      }),
+      new CopyWebpackPlugin([
+        // relative path is from src
+        { from: './app/src/assets/favicon.ico' }
+      ])
+    ],
+    module: {
+      rules: [
+        {   
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: env.NODE_ENV === 'production' ?
+                ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: 'css-loader'
+                }) :
+                'style-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'css-loader',
+              options: { 
+                sourceMap: true,
+                importLoaders: 1 
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true }
             }
-          },
-          {
-            loader: 'postcss-loader',
-            options: { sourceMap: true }
+          ]
+        },
+        {
+          test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+          use: {
+            loader: 'url-loader',
+            options: { limit: 5000 },
           }
-        ]
-      },
-      {
-        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
-        use: {
-          loader: 'url-loader',
-          options: { limit: 5000 },
         }
-      }
-    ]
-  }
+      ]
+    }
+  };
 };
