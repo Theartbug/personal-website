83f93ec92f624c0c3b7e60ce49df62ec870c5bcc
G.Provost <grace.g.provost@gmail.com>
2018-12-09 20:29:36 -0800

    change from arrow to function

--- webpack.config.js
+++ webpack.config.js
@@ -9,77 +9,79 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 console.log('node env', process.env.NODE_ENV);
 
-module.exports = env => ({
-  entry: './app/src/main.js',
-  output: {
-    path,
-    filename: 'bundle.[hash].js',
-    publicPath: ''
-  },
-  devServer: {
-    contentBase: './build',
-    historyApiFallback: true
-  },
-  devtool: env === 'production' ?
-    'cheap-eval-source-map' :
-    'inline-source-map',
-  plugins: [
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     // defaults the environment to development if not specified
-    //     'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    //   }
-    // }),
-    new ExtractTextPlugin('styles.css'),
-    new CleanWebpackPlugin(`${path}/bundle.*.js`),
-    new HtmlPlugin({ template: './app/src/index.html' }),
-    new Dotenv({
-      systemvars: true //allows netlify system variables to work in production
-    }),
-    new CopyWebpackPlugin([
-      // relative path is from src
-      { from: './app/src/assets/favicon.ico' }
-    ])
-  ],
-  module: {
-    rules: [
-      {   
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: env === 'production' ?
-              ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: 'css-loader'
-              }) :
-              'style-loader',
-            options: { sourceMap: true }
-          },
-          {
-            loader: 'css-loader',
-            options: { 
-              sourceMap: true,
-              importLoaders: 1 
+module.exports = function(env) {
+  return {
+    entry: './app/src/main.js',
+    output: {
+      path,
+      filename: 'bundle.[hash].js',
+      publicPath: ''
+    },
+    devServer: {
+      contentBase: './build',
+      historyApiFallback: true
+    },
+    devtool: env === 'production' ?
+      'cheap-eval-source-map' :
+      'inline-source-map',
+    plugins: [
+      // new webpack.DefinePlugin({
+      //   'process.env': {
+      //     // defaults the environment to development if not specified
+      //     'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      //   }
+      // }),
+      new ExtractTextPlugin('styles.css'),
+      new CleanWebpackPlugin(`${path}/bundle.*.js`),
+      new HtmlPlugin({ template: './app/src/index.html' }),
+      new Dotenv({
+        systemvars: true //allows netlify system variables to work in production
+      }),
+      new CopyWebpackPlugin([
+        // relative path is from src
+        { from: './app/src/assets/favicon.ico' }
+      ])
+    ],
+    module: {
+      rules: [
+        {   
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: env === 'production' ?
+                ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: 'css-loader'
+                }) :
+                'style-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'css-loader',
+              options: { 
+                sourceMap: true,
+                importLoaders: 1 
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: { sourceMap: true }
             }
-          },
-          {
-            loader: 'postcss-loader',
-            options: { sourceMap: true }
+          ]
+        },
+        {
+          test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+          use: {
+            loader: 'url-loader',
+            options: { limit: 5000 },
           }
-        ]
-      },
-      {
-        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
-        use: {
-          loader: 'url-loader',
-          options: { limit: 5000 },
         }
-      }
-    ]
-  }
-});
+      ]
+    }
+  };
+};
