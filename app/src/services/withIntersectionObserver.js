8ba56fa35b8d24d4e249aa4df4019a552ad17ab8
G.Provost <grace.g.provost@gmail.com>
2019-02-23 14:41:29 -0800

    start converting withIntersectionObserver

--- app/src/services/withIntersectionObserver.js
+++ app/src/services/withIntersectionObserver.js
@@ -1,38 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Observer from 'react-intersection-observer';
+import { useInView } from 'react-intersection-observer';
 import { setCurrentSectionByScroll } from '../components/scroll-buttons/actions';
 
-export default BaseComponent => {
+export default function BaseComponent(props) {
   const displayName = BaseComponent.displayName || BaseComponent.name || 'Component';
+  
+  function WithIntersectionObserver() {
+    const [ref, inView] = useInView({ rootMargin: '-10% 0px -55%' });
 
-  class WithIntersectionObserver extends Component {
-    static displayName = `withIntersectionObserver(${displayName})`;
+    function handleChange() {
+      const { buttonScroll, setCurrentSectionByScroll } = props;
 
-    state = {
-      isIntersecting: false,
+      if(!buttonScroll) setCurrentSectionByScroll(displayName.toLowerCase());
     };
 
-    handleChange = (isIntersecting) => {
+    if(inView) handleChange();
 
-      const { buttonScroll, setCurrentSectionByScroll } = this.props;
-
-      if(!buttonScroll && isIntersecting) setCurrentSectionByScroll(displayName.toLowerCase());
-    };
-
-    render() {
-
-      const options = {
-        onChange: this.handleChange,
-        rootMargin: '-10% 0px -55%',
-      };
-      return (
-        <Observer {...options}>
-          <BaseComponent {...this.props} isVisible={this.state.isIntersecting} />
-        </Observer>
-      );
-    }
+    return <BaseComponent ref={ref} {...props}/>;
+    
   }
+
+  WithIntersectionObserver.displayName = `withIntersectionObserver(${displayName})`;
 
   return connect(
     ({ buttonScroll }) => ({
