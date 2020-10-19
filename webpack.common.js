8bed611cd078a688778272e5adccfe4476e8353d
Ash Provost <provost@adobe.com>
2020-10-19 10:39:46 -0700

    it runs, still unstable, no styles

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = `${__dirname}/build`;
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
@@ -14,10 +14,10 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(`${path}`),
     new HtmlPlugin({ template: './app/src/index.html' }),
-    new CopyWebpackPlugin([
+    new CopyPlugin({
       // relative path is from src
-      { from: './app/src/assets/favicon.ico' }
-    ]),
+      patterns: [{ from: './app/src/assets/favicon.ico'}]
+    }),
     new Dotenv({
       systemvars: true //allows netlify system variables to work in production
     }),
