631b418689d316779b80c95092fecd1a3400e12d
Ash Provost <provost@adobe.com>
2020-10-20 09:23:54 -0700

    fix build with update

--- webpack.prod.js
+++ webpack.prod.js
@@ -28,7 +28,7 @@ module.exports = merge(common, {
             loader: 'css-loader',
             options: {
               sourceMap: true,
-              importLoaders: 1
+              importLoaders: 1,
             }
           },
           {
