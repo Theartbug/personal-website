8f6f868aa7a58131c17b24f85326b6d79d8b3a42
Ash Provost <provost@adobe.com>
2020-11-17 09:19:22 -0800

    add types to middlware

--- src/components/app-context/AppContext.tsx
+++ src/components/app-context/AppContext.tsx
@@ -1,10 +1,11 @@
 import React, { useReducer, useMemo, useContext } from 'react';
 import { reducer, Action } from './reducers.js';
 
+type dispatchType = React.Dispatch<Action>;
 interface AppContextInterface {
   buttonScroll: boolean;
   currentSection: number;
-  dispatch: React.Dispatch<Action>;
+  dispatch: Function;
 }
 export const initialState = {
   buttonScroll: false,
@@ -15,11 +16,12 @@ export type stateType = typeof initialState;
 // { Provider, Consumer }
 const Context = React.createContext<stateType>(initialState);
 
-
 // Thunk middleware replacement
-const augmentDispatch = (dispatch: React.Dispatch<Action>, state: stateType): Function =>
-  (input: Function | Action) =>
-    input instanceof Function ? input(dispatch, state) : dispatch(input);
+const middleware = (dispatch: dispatchType, state: stateType): Function =>
+  (next: Function | Action): Function | dispatchType =>
+    next instanceof Function
+      ? next(dispatch, state)
+      : dispatch(next);
 
 function AppContext({ children }) {
   const [state, dispatch] = useReducer<React.Reducer<stateType, Action>>(reducer, initialState);
@@ -28,10 +30,10 @@ function AppContext({ children }) {
   // ^^^^^^ DON'T DO as react uses object.is() for reference comparisons, new object could possibly be created each time and trigger unnecessary re-renders
   //https://reactjs.org/docs/context.html#caveats
   // instead useMemo so that the object is only changed when currentSection or buttonScroll changes
-  const value = useMemo(() => ({
+  const value = useMemo<AppContextInterface>(() => ({
       buttonScroll,
       currentSection,
-      dispatch: augmentDispatch(dispatch, state) // now supports thunks
+      dispatch: middleware(dispatch, state) // now supports thunks
     }),
     [buttonScroll, currentSection]);
   return (
