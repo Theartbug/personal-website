a9faa828b1e583acdc75b70d1113877227a00501
Ash Provost <provost@adobe.com>
2020-11-24 08:45:00 -0800

    add types to actions

--- src/components/app-context/actions.ts
+++ src/components/app-context/actions.ts
@@ -9,9 +9,27 @@ export const CONTACT = 'contact';
 export const PROJECTS = 'projects';
 export const GALLERY = 'gallery';
 
-const list = [HERO, BIO, SKILLS, GITHUB, PROJECTS, GALLERY, CONTACT];
+type appAreaIds = typeof HERO
+  | typeof BIO
+  | typeof SKILLS
+  | typeof GITHUB
+  | typeof PROJECTS
+  | typeof GALLERY
+  | typeof CONTACT;
 
-const scrollTo = (id) => {
+type orderedList = [
+  typeof HERO,
+  typeof BIO,
+  typeof SKILLS,
+  typeof GITHUB,
+  typeof PROJECTS,
+  typeof GALLERY,
+  typeof CONTACT,
+];
+
+const list: orderedList = [HERO, BIO, SKILLS, GITHUB, PROJECTS, GALLERY, CONTACT];
+
+const scrollTo = (id: appAreaIds): void => {
   if(!id) return;
   scroller.scrollTo(id, {
     duration: 1000,
@@ -20,40 +38,38 @@ const scrollTo = (id) => {
   });
 };
 
-export const setCurrentSectionByButtons = (section: number) => (dispatch: middlewareType) => {
+export const setCurrentSectionByButtons = (section: number) => (dispatch: middlewareType): void => {
   scrollTo(list[section]);
   dispatch(setButtonScroll(true));
   dispatch(setCurrentSection(section));
 };
 
-export const setCurrentSectionByScroll = section => {
+export const setCurrentSectionByScroll = (section: appAreaIds) => {
   const current = list.indexOf(section);
   return setCurrentSection(current);
 };
 
 export const BUTTON_PRESS = 'BUTTON_PRESS';
-type ButtonScrollType = {
+type buttonScrollType = {
   type: typeof BUTTON_PRESS;
   payload: boolean;
 };
-
-export const setButtonScroll = (payload: boolean): ButtonScrollType => ({
+export const setButtonScroll = (payload: boolean): buttonScrollType => ({
   type: BUTTON_PRESS,
   payload
 });
 
 export const SECTION_CHANGE = 'SECTION_CHANGE';
-type CurrentSectionType = {
+type currentSectionType = {
   type: typeof SECTION_CHANGE;
   payload: number;
 };
-
-const setCurrentSection = (payload: number): CurrentSectionType => ({
+const setCurrentSection = (payload: number): currentSectionType => ({
   type: SECTION_CHANGE,
   payload
 });
 
 
-export type Action = CurrentSectionType | ButtonScrollType;
+export type Action = currentSectionType | buttonScrollType;
 
 
