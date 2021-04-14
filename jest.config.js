4e7d8a48e7dbf1972f123d97c1539fb8d29a38f2
Ash Provost <provost@adobe.com>
2021-04-14 11:07:05 -0700

    add config files to setup

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   "moduleNameMapper": {
     "\\.(css|scss)$": "identity-obj-proxy",
     "\\.(png|svg|pdf|jpg|jpeg)$": "<rootDir>/src/tests/mocks/fileMock.js"
-}
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
 }
